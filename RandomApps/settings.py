af1e1daa52cb81b92bd45ff70d33bf5b375f143e
shoko-nurs <shokonurs@gmail.com>
2022-08-03 14:06:49 +0600

    edited

--- RandomApps/settings.py
+++ RandomApps/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -167,3 +168,4 @@ STATICFILES_DIRS=[
 AUTH_USER_MODEL = 'Users.CustomUser'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
