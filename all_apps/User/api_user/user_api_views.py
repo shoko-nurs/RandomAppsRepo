043a10e35e4e94ab8206e5c95af67921362de0c5
shoko-nurs <shokonurs@gmail.com>
2022-08-05 15:37:56 +0600

    Registration form CSS+JS+Fetch is complete

--- all_apps/User/api_user/user_api_views.py
+++ all_apps/User/api_user/user_api_views.py
@@ -1,8 +1,10 @@
+from unittest import result
 from rest_framework import generics
 from ..models import CustomUser
 from .user_serializers import RegistrationSerializer
 from rest_framework.response import Response
 from django.conf import settings
+from django.contrib.auth import password_validation
 import re
 
 
@@ -28,13 +30,14 @@ class RegistrationAPIView(generics.GenericAPIView):
         return Response({'ok':'ok'})
 
 
-class EmailControlFetch(generics.GenericAPIView):
-
-
+class EmailControl(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         data = request.data
+        
         email = data['email']
+        if not email:
+            return Response({'message':'Email can not be blank'})
 
         if not validate_email(email):
             return Response({"message":"Enter valid email address"})
@@ -43,5 +46,39 @@ class EmailControlFetch(generics.GenericAPIView):
         if qs.exists():
             return Response({"message":'This email is already registered'})
         
-        return Response({'message':'This email is available'})
+        return Response({'message':'OK'})
+
+
+class Password1Control(generics.GenericAPIView):
     
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        password = data['password1']
+        
+        
+        if not password or password=="":
+            return Response({"message":["Password can not be blank"]})
+        
+        try:
+            password_validation.validate_password(password) 
+            
+            return Response({"message":"OK"})
+
+        except Exception as e:
+            return Response({"message":e})
+
+
+class Password2Control(generics.GenericAPIView):
+
+    def post(sellf, request, *args, **kwargs):
+        data = request.data
+        password1 = data['password1']
+        password2 = data['password2']
+
+        if not password2:
+            return Response({"message":"Confirm your password"})
+        
+        if password1!=password2:
+            return Response({"message":"Passwords must match"})
+        
+        return Response({"message":"OK"})
--- all_apps/User/user_urls.py
+++ all_apps/User/user_urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .user_views import RegistrationView
-from .api_user.user_api_views import RegistrationAPIView, EmailControlFetch
+from .api_user.user_api_views import (
+    RegistrationAPIView, 
+    EmailControl,
+    Password1Control,
+    Password2Control)
+
 
 urlpatterns=[
 
@@ -8,5 +13,8 @@ urlpatterns=[
     path('', RegistrationView.as_view(), name='register'),
     path('api/registration/', RegistrationAPIView.as_view(), name='registration_api'),
     
-    path('api/email_control_fetch/', EmailControlFetch.as_view(), name='email_control_fetch'),
+    path('api/email_control_fetch/', EmailControl.as_view(), name='email_control_fetch'),
+    path('api/password1_contol_fetch/', Password1Control.as_view() , name='password1_control_fetch'),
+    path('api/password2_control_fetch/', Password2Control.as_view(), name='password2_control_fetch'),
+    
 ]
