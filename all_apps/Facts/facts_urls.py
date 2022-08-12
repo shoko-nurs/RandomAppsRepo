5aa815313c28520023f090b5c96ae8ab359fa15e
shoko-nurs <shokonurs@gmail.com>
2022-08-12 16:11:44 +0600

    Good work

--- all_apps/Facts/facts_urls.py
+++ all_apps/Facts/facts_urls.py
@@ -1,37 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .facts_views import FactsMainPageView, UserFactsPage
-from .api.facts_api_views import (
-    FactsAPIVIew, 
-    NursFromCategoryApiView,
-    UserCategoriesFetch,
-    EditCategoryFetch,
-    EditCategoryFetch,
-    AddCategoryFetch,
-    GetFactsFromCatFetch,
-    EditFactFetch,
-    AddFactFetch,
-    GetFirstFacts,
-    DeleteFactFetch,
-    TestFactFetch
-    
-)
-
 
 urlpatterns=[
 
     path('', FactsMainPageView.as_view(), name='facts-main-page'),
     path('user/', UserFactsPage.as_view(),name="user_facts_page" ),
 
-    path('api/all_facts', FactsAPIVIew.as_view(), name='facts-list' ),
-    path('api/from/', NursFromCategoryApiView.as_view(), name='from_category' ),
-    path('api/user_categories/', UserCategoriesFetch.as_view(), name='user_categories_fetch'),
-    path('api/edit_category/', EditCategoryFetch.as_view(), name='edit_category_fetch'),
-    path('api/add_category_fetch', AddCategoryFetch.as_view(), name="add_category_fetch"),
-    path('api/get_facts_from_cat_fetch',GetFactsFromCatFetch.as_view(), name='get_facts_from_cat_fetch'),
-    path('api/edit_fact_fetch', EditFactFetch.as_view(), name="edit_fact_fetch"),
-    path('api/add_fact_fetch', AddFactFetch.as_view(), name='add_fact_fetch'),
-    path('api/get_first_facts', GetFirstFacts.as_view(), name="get_first_facts"),
-    path('api/delete_fact_fetch',DeleteFactFetch.as_view(), name='delete_fact_fetch'),
-    path('api/test_fact', TestFactFetch.as_view(), name="test_fact_fetch"),
+    path('fetch_api/', include('all_apps.Facts.fetch_api.fetch_urls')),
+
 
 ]
---
+++ all_apps/Facts/fetch_api/fetch_urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from .facts_api_views import *
+
+
+urlpatterns = [
+
+        path('all_facts', FactsAPIVIew.as_view(), name='facts-list' ),
+        path('from/', NursFromCategoryApiView.as_view(), name='from_category' ),
+        path('user_categories/', UserCategoriesFetch.as_view(), name='user_categories_fetch'),
+        path('edit_category/', EditCategoryFetch.as_view(), name='edit_category_fetch'),
+        path('add_category_fetch', AddCategoryFetch.as_view(), name="add_category_fetch"),
+        path('get_facts_from_cat_fetch',GetFactsFromCatFetch.as_view(), name='get_facts_from_cat_fetch'),
+        path('edit_fact_fetch', EditFactFetch.as_view(), name="edit_fact_fetch"),
+        path('add_fact_fetch', AddFactFetch.as_view(), name='add_fact_fetch'),
+        path('get_first_facts', GetFirstFacts.as_view(), name="get_first_facts"),
+        path('delete_fact_fetch',DeleteFactFetch.as_view(), name='delete_fact_fetch'),
+        path('test_fact', TestFactFetch.as_view(), name="test_fact_fetch"),
+
+
+]
